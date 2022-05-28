514f73d14e4606c76f26cac71fffb1ceeda16719
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-05-28 17:06:13 -0300

    test: testing logo colors.

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,27 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
+// Por padrao o compontende deve renderizar na cor branca
+// Renderizar o componente `render`
+// Selecionar o elemento a ser testado `screen` (queries) = getByLabel ...
+// Expect - assertion - assercao - comparacao - analise (espero que renderize a logo na cor branca)
+
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('should render a white label by default', () => {
+    renderWithTheme(<Logo />)
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a black labelwhen is passed', () => {
+    renderWithTheme(<Logo color="black" />)
+
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
