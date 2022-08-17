f0e22f2a527977576811bda352b96c23a2622251
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-08-17 00:50:19 -0300

    feat(Button): add button component.

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import * as Styled from './styles'
+
+export type ButtonProps = {
+  children?: React.ReactNode
+  size?: 'small' | 'medium' | 'large'
+}
+
+const Button = ({ children, size = 'medium' }: ButtonProps) => (
+  <Styled.Wrapper size={size}>
+    {!!children && <span>{children}</span>}
+  </Styled.Wrapper>
+)
+
+export default Button
