a9d132a9f6b5f1a644a10c11a9587698c57ff85c
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-08-20 00:06:55 -0300

    feat(Heading): adding variations to the heading

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,19 +1,31 @@
 import * as Styled from './styles'
 
+export type LineColors = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  lineColor?: LineColors
+  size?: 'small' | 'medium'
 }
 
 const Heading = ({
   children,
   color = 'white',
   lineLeft = false,
-  lineBottom = false
+  lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium'
 }: HeadingProps) => (
-  <Styled.Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+  <Styled.Wrapper
+    color={color}
+    lineLeft={lineLeft}
+    lineBottom={lineBottom}
+    lineColor={lineColor}
+    size={size}
+  >
     {children}
   </Styled.Wrapper>
 )
