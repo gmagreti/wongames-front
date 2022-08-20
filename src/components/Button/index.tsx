c737dd43e7a949e18e49d183fb085c274e86c357
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-08-19 22:41:22 -0300

    feat(Button): adding onClick button.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,15 +5,17 @@ export type ButtonProps = {
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
   icon?: JSX.Element
+  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const Button = ({
   children,
   icon,
   size = 'medium',
-  fullWidth = false
+  fullWidth = false,
+  ...props
 }: ButtonProps) => (
-  <Styled.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon}>
+  <Styled.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
     {!!icon && icon}
     {!!children && <span>{children}</span>}
   </Styled.Wrapper>
