19fa1da798067a65f6756babb4dd5b16bab01a8a
Gabriel Magreti <gabrielmagreti@gmail.com>
2022-08-20 00:25:31 -0300

    feat(Footer): finalizing footer structuring

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,87 @@
 import * as Styled from './styles'
+import Link from 'next/link'
+
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
 
 const Footer = () => (
   <Styled.Wrapper>
-    <h1>Footer</h1>
+    <Logo color="black" />
+    <Styled.Content>
+      <Styled.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Contact Us
+        </Heading>
+
+        <a href="mailto:sac@wongameStyled.com">sac@wongameStyled.com</a>
+      </Styled.Column>
+
+      <Styled.Column>
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Follow us
+        </Heading>
+
+        <nav aria-labelledby="social media">
+          <a
+            href="https://www.instagram.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            href="https://www.twitter.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Twitter
+          </a>
+          <a
+            href="https://www.youtube.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Youtube
+          </a>
+          <a
+            href="https://www.facebook.com/won-games"
+            target="_blank"
+            rel="noopenner, noreferrer"
+          >
+            Facebook
+          </a>
+        </nav>
+      </Styled.Column>
+
+      <Styled.Column>
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Links
+        </Heading>
+
+        <nav aria-labelledby="footer resources">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/games">
+            <a>Store</a>
+          </Link>
+          <Link href="/search">
+            <a>Buscar</a>
+          </Link>
+        </nav>
+      </Styled.Column>
+
+      <Styled.Column aria-labelledby="footer-contact">
+        <Heading color="black" lineColor="secondary" lineBottom size="small">
+          Location
+        </Heading>
+        <span>Lorem ipsum dolor sit.</span>
+        <span>Lorem Ipsum</span>
+        <span>Lorem, ipsum dolor.</span>
+      </Styled.Column>
+    </Styled.Content>
+
+    <Styled.Copyright>Won Games 2020 © All rights reserved.</Styled.Copyright>
   </Styled.Wrapper>
 )
 
